42844401620ca8f6bb66efe929573713a9a10207
johnny kang <johnny kang>
2016-08-23 07:16:43 +0900

    A method for improving the trigger generation

--- modules/paynoty/paynoty.class.php
+++ modules/paynoty/paynoty.class.php
@@ -7,76 +7,10 @@
  */
 class paynoty extends ModuleObject
 {
-	/**
-	 * @brief Object를 텍스트의 %...% 와 치환.
-	 **/
-	function mergeKeywords($text, &$obj)
-	{
-		if(!is_object($obj))
-		{
-			return $text;
-		}
+	private $triggers = array(
+		'epay.processPayment', 'paynoty', 'controller', 'triggerCompletePayment', 'after'
+	);
 
-		foreach($obj as $key => $val)
-		{
-			if(is_array($val))
-			{
-				$val = join($val);
-			}
-			if(is_numeric($val))
-			{
-				$val = (string)$val;
-			}
-			if(is_string($key) && is_string($val))
-			{
-				if($key == 'state')
-				{
-					switch($val)
-					{
-						case '1' :
-							$val = '결제 진행중';
-							break;
-						case '2' :
-							$val = '결제 완료';
-							break;
-						case '3' :
-							$val = '결제 오류';
-							break;
-					}
-				}
-				if($key == 'payment_method')
-				{
-					switch($val)
-					{
-						case 'CC' :
-							$val = '신용 카드';
-							break;
-						case 'BT' :
-							$val = '무통장 입금';
-							break;
-						case 'IB' :
-							$val = '실시간 계좌 이체';
-							break;
-						case 'VA' :
-							$val = '가상 계좌';
-							break;
-						case 'MP' :
-							$val = '휴대폰 소액결제';
-							break;
-						case 'PP' :
-							$val = '페이팔';
-							break;
-					}
-				}
-				if(substr($key, 0, 10) == 'extra_vars')
-				{
-					$val = str_replace('|@|', '-', $val);
-				}
-				$text = preg_replace("/%" . preg_quote($key) . "%/", $val, $text);
-			}
-		}
-		return $text;
-	}
 
 	/**
 	 * @brief 모듈 설치 실행
@@ -86,8 +20,13 @@ class paynoty extends ModuleObject
 		$oModuleController = getController('module');
 		$oModuleModel = getModel('module');
 
-		// Document Registration Trigger
-		$oModuleController->insertTrigger('epay.processPayment', 'paynoty', 'controller', 'triggerCompletePayment', 'after');
+		foreach($this->triggers as $trigger)
+		{
+			if(!$oModuleModel->getTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]))
+			{
+				$oModuleController->insertTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]);
+			}
+		}
 	}
 
 	/**
@@ -97,14 +36,14 @@ class paynoty extends ModuleObject
 	{
 		$oDB = &DB::getInstance();
 		$oModuleModel = getModel('module');
-		$oModuleController = getController('module');
 
-		// Document Registration Trigger
-		if(!$oModuleModel->getTrigger('epay.processPayment', 'paynoty', 'controller', 'triggerCompletePayment', 'after'))
+		foreach($this->triggers as $trigger)
 		{
-			return true;
+			if(!$oModuleModel->getTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]))
+			{
+				return TRUE;
+			}
 		}
-
 		if(!$oDB->isColumnExists('paynoty_config', 'sender_no'))
 		{
 			return TRUE;
@@ -121,10 +60,12 @@ class paynoty extends ModuleObject
 		$oModuleModel = getModel('module');
 		$oModuleController = getController('module');
 
-		// Document Registration Trigger
-		if(!$oModuleModel->getTrigger('epay.processPayment', 'paynoty', 'controller', 'triggerCompletePayment', 'after'))
+		foreach($this->triggers as $trigger)
 		{
-			$oModuleController->insertTrigger('epay.processPayment', 'paynoty', 'controller', 'triggerCompletePayment', 'after');
+			if(!$oModuleModel->getTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]))
+			{
+				$oModuleController->insertTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]);
+			}
 		}
 
 		if(!$oDB->isColumnExists('paynoty_config', 'sender_no'))
