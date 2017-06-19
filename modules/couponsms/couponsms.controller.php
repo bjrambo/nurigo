f483254492014be368187218593ca5be6ec2f8df
BJRambo <qw5414@naver.com>
2017-06-19 16:56:20 +0900

    Deleted comment code

--- modules/couponsms/couponsms.controller.php
+++ modules/couponsms/couponsms.controller.php
@@ -8,7 +8,6 @@ class couponsmsController extends couponsms
 
 	function procCouponsmsSendMessage()
 	{
-		/*
 		$oMemberModel = getModel('member');
 		$oCouponsmsModel = getModel('couponsms');
 		$logged_info = Context::get('logged_info');
@@ -135,7 +134,6 @@ class couponsmsController extends couponsms
 			header('location: ' . $returnUrl);
 			return;
 		}
-		*/
 	}
 
 	public static function sendMessage($phone_number, $r_number, $content, $title)
