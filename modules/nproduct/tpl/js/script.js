c5448b4a7700890678478b1fb49a6ab27721170c
BJRambo <qw5414@naver.com>
2019-01-24 16:19:09 +0900

    글 수정시 분류 선택 문제 고침

--- modules/nproduct/tpl/js/script.js
+++ modules/nproduct/tpl/js/script.js
@@ -167,8 +167,8 @@ function load_categories(module_srl, node_id, target) {
 			if (r.error == -1) {
 				alert(r.message);
 			} else {
-				for (i = 0; i < r.data.length; i++) {
-					jQuery('<option value="' + r.data[i].attr.node_id + '">' + r.data[i].attr.node_name + '</option>').appendTo(target);
+				for (i = 0; i < r.length; i++) {
+					jQuery('<option value="' + r[i].attr.node_id + '">' + r[i].attr.node_name + '</option>').appendTo(target);
 				}
 			}
 		}
